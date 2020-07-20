2eab962d0b3d4d1b9084d099983c4a536a6e1f1b
Pratik Bhattacharya <patrickb404@outlook.com>
2020-07-20 21:21:25 +0530

    Added context and policy wrapping

--- Polly/Polly/Controllers/CatalogController.cs
+++ Polly/Polly/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
@@ -8,6 +9,7 @@ using Polly;
 using Polly.Fallback;
 using Polly.Retry;
 using Polly.Timeout;
+using Polly.Wrap;
 
 namespace PollySample.Controllers
 {
@@ -18,24 +20,30 @@ namespace PollySample.Controllers
         private readonly AsyncRetryPolicy<HttpResponseMessage> _httpWaitAndRetryPolicy;
         private readonly AsyncFallbackPolicy<HttpResponseMessage> _fallbackPolicy;
         private readonly AsyncTimeoutPolicy _timeoutPolicy;
+        private readonly AsyncPolicyWrap<HttpResponseMessage> _policyWrap;
 
-        public CatalogController() 
+        public CatalogController()
         {
             _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => result.StatusCode == System.Net.HttpStatusCode.InternalServerError)
                 .Or<TimeoutRejectedException>()
-                .RetryAsync(2);
-            
+                .RetryAsync(3);
+
             _httpWaitAndRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
                 .WaitAndRetryAsync(3, sleepDurationProvider: retryCount =>
                     TimeSpan.FromSeconds(Math.Pow(2, retryCount) / 2),
-                    onRetry: (httpResponseMessage, time) =>
+                    onRetry: (httpResponseMessage, time, context) =>
                     {
                         if (httpResponseMessage.Result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                         {
                             Debug.WriteLine("There was issue with authentication");
                         }
                         else
-                            Debug.WriteLine(httpResponseMessage.Result.Content.ReadAsStringAsync().Result);
+                        {
+                            if (context.Contains("Logging") && context["Logging"].ToString() == "Enabled")
+                            {
+                                Debug.WriteLine(httpResponseMessage.Result.Content.ReadAsStringAsync().Result);
+                            }
+                        }
                     });
 
             _fallbackPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
@@ -45,6 +53,8 @@ namespace PollySample.Controllers
                 });
 
             _timeoutPolicy = Policy.TimeoutAsync(seconds: 1);
+
+            _policyWrap = _fallbackPolicy.WrapAsync(_httpWaitAndRetryPolicy.WrapAsync(_timeoutPolicy));
         }
 
         [HttpGet]
@@ -53,6 +63,11 @@ namespace PollySample.Controllers
         {
             var httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("https://localhost:44363/");
+            var contextDictionary = new Dictionary<string, object>
+            {
+                { "Logging", "Enabled" }
+            };
+            var catalogContext = new Context("CatalogContext", contextDictionary);
 
             // Without polly
             //var response = await httpClient.GetAsync($"api/inventories/{productId}");
@@ -67,11 +82,17 @@ namespace PollySample.Controllers
             // var response = await _fallbackPolicy.ExecuteAsync(async () => 
             //     await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync($"api/inventories/{productId}")));
 
-            // With Polly (Fallback, Retry and timeout)
-            var response = await
-                    _fallbackPolicy.ExecuteAsync(() =>
-                        _httpRetryPolicy.ExecuteAsync(() =>
-                            _timeoutPolicy.ExecuteAsync(async (token) => await httpClient.GetAsync($"api/inventories/{productId}", token), cancellationToken: CancellationToken.None)));
+            // With Polly (Fallback, Retry and timeout) - Manual Wrap
+            //var response = await
+            //        _fallbackPolicy.ExecuteAsync(() =>
+            //            _httpRetryPolicy.ExecuteAsync(() =>
+            //                _timeoutPolicy.ExecuteAsync(async (token) => await httpClient.GetAsync($"api/inventories/{productId}", token), cancellationToken: CancellationToken.None)));
+
+            // With Polly and Wrap
+            // var response = await _policyWrap.ExecuteAsync(() => httpClient.GetAsync($"api/inventories/{productId}", CancellationToken.None));
+
+            // With Polly and context
+            var response = await _policyWrap.ExecuteAsync((context) => httpClient.GetAsync($"api/inventories/{productId}", CancellationToken.None), catalogContext);
 
             var result = await response.Content.ReadAsStringAsync();
             return new OkObjectResult(result);
